ccaa7602f7f5dd5db0b202ad283f6728b6e70540
cheqitout-dev <cheqitoutdev@gmail.com>
2016-12-17 22:17:34 -0800

    Adding terrain assets, putting buildings together

---
+++ Assets/cameraRay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraRay : MonoBehaviour {
+    public Camera camera;
+
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+            try
+            {
+                hit.transform.parent.gameObject.active = true;
+            }
+            catch
+            {
+                print("No parent");
+            }
+            // Do something with the object that was hit by the raycast.
+        }
+    }
+}
